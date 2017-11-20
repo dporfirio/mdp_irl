431054a035dfa9e1dcd2cf8e2c8e5ed05d75f5f8
David Porfirio <dporfirio@wisc.edu>
2017-11-20 13:42:14 -0600

    added MDP file

---
+++ MDP.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class MDP():
+
+	def __init__(statelist, actionlist):
+		mdp = np.zeros((len(statelist), len(actionlist), len(statelist)))
+
+class State():
+
+	def __init__():
+		pass
+
+class Action():
+
+	def __init__():
+		pass
