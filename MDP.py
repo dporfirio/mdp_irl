0a5d95e9490c2da073aef22dc34bf4c5d9937553
David Porfirio <dporfirio@wisc.edu>
2017-11-21 09:29:51 -0600

    added more structure to the MDP file

--- MDP.py
+++ MDP.py
@@ -1,9 +1,18 @@
 import numpy as np
+from scipy.optimize import maximize
 
 class MDP():
 
 	def __init__(statelist, actionlist):
-		mdp = np.zeros((len(statelist), len(actionlist), len(statelist)))
+		mdp = np.zeros((len(actionlist), len(statelist), len(statelist)))
+		action_idx = {}
+		for i in range(0,len(actionlist)):
+			action_idx[actionlist[i]] = i
+		state_idx = {}
+		for i in range(0, len(statelist)):
+			state_idx[statelist[i]] = i
+
+
 
 class State():
 
@@ -13,4 +22,21 @@ class State():
 class Action():
 
 	def __init__():
+		pass
+
+class IRL():
+
+	def __init__(mdp, R=np.zeros):
+		pass
+
+	# # # # # # # # # # # #
+	# finite state spaces #
+	# # # # # # # # # # # #
+	def obj(x):
+		pass
+
+	def constraint():
+		pass
+
+	def solve():
 		pass
